3bb076c27692e7565859c17c50915a19c37035b6
Jocelyne Jones <gothling@jocely.net>
2024-08-06 14:24:29 +0100

    scores for all letters

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,8 +1,21 @@
 export function calculateScrabbleScore(word) {
   const onePointLetters = ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"]
-  if (onePointLetters.includes(word)) {
-    return(1);
-  } else {
-    return(0)
+  const bigLetterArray = [
+    ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"],
+    ["D", "G"],
+    ["B", "C", "M", "P"],
+    ["F", "H", "V", "W", "Y"],
+    ["K"],
+    [],
+    [],
+    ["J", "X"],
+    [],
+    ["Q", "Z"]
+  ]
+  for (let i = 0; i < bigLetterArray.length; i++){
+    if (bigLetterArray[i].includes(word)){
+      return(i+1);
+    }
   }
+  return(0);
 }
