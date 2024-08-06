bd8a3a15aebe7f72d513a7314b14d73886ba44ab
Jocelyne Jones <gothling@jocely.net>
2024-08-06 13:58:32 +0100

    more letters

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,1 +1,6 @@
-export function calculateScrabbleScore(word) {}
+export function calculateScrabbleScore(word) {
+  const onePointLetters = ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"]
+  if (onePointLetters.includes(word)) {
+    return(1);
+  }
+}
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,6 +1,26 @@
 import { calculateScrabbleScore } from "./scrabble-score";
 import { expect, test } from 'vitest'
 
-test('adds 1 + 2 to equal 3', () => {
-  
+/*
+test('A', () => {
+  expect.soft(calculateScrabbleScore("A")).toBe(1)
   })
+*/
+
+test.each(
+  [ "A", 1],
+  [ "A", 1],
+  [ "B", 3],
+  [ "E", 1],
+  [ "I", 1],
+  [ "L", 1],
+  [ "N", 1],
+  [ "O", 1],
+  [ "R", 1],
+  [ "S", 1],
+  [ "T", 1],
+  [ "U", 1])(
+  'calculateScrabbleScore(%s) => %i', (a, expected) => {
+    expect(calculateScrabbleScore(a)).toBe(expected)
+  }
+)
