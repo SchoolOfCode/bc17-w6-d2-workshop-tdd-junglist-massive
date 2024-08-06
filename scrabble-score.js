5021aa720ce3b2625bc5c4d3905332381d7d1941
Jocelyne Jones <gothling@jocely.net>
2024-08-06 14:08:37 +0100

    added test for full alphabet

--- scrabble-score.js
+++ scrabble-score.js
@@ -2,5 +2,7 @@ export function calculateScrabbleScore(word) {
   const onePointLetters = ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"]
   if (onePointLetters.includes(word)) {
     return(1);
+  } else {
+    return(0)
   }
 }
